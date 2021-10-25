b5f426af2dfd99ffcc5136c966d3297cb7bea11a
caiolombello <caiolvbarbieri@pm.me>
2021-10-25 19:56:08 -0300

    maior/igual

--- Exemplo/src/main/java/ex2.java
+++ Exemplo/src/main/java/ex2.java
@@ -7,15 +7,19 @@ public class ex2 {
         Scanner teclado = new Scanner(System.in);
         double[] nums = new double[10];
         int maior = 0;
+        int igual = 0;
 
         for(int i = 0; i < 10; i++){
             System.out.println("Digite um número: ");
             nums[i] = teclado.nextInt();
 
-            if (nums[i] >= 100){
+            if (nums[i] > 100){
                 maior += 1;
             }
+            else if (nums[i] == 100){
+                igual += 1;
+            }
         }
-        System.out.printf("Dos números digitados, os maiores e iguais a 100 são: %d", maior);
+        System.out.printf("Dos números digitados:\nMaiores que 100 são: %d\nIguais a 100 são: %d", maior, igual);
     }
 }
